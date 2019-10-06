3e790740bae883038b4d3dae946028a42921e23e
Liam Maclachlan <dev_lime@protonmail.com>
2019-10-06 09:21:41 +0100

    Removed references to old models

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,14 @@
 <template>
   <div class="hello">
-    <h1>{{ user }}</h1>
-    <h2>{{ users.getItems() }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import User from '@/models/User'
-import UserCollection from '@/models/UserCollection'
-
+import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
-  @Prop({default: () => new User('', '')})
-  user!: User;
-
-  @Prop({default: () => {
-    const items = new UserCollection();
-    for(let i = 0; i <=5; i += 1 ) {
-      items.addItem(new User('', ''));
-    }
-    return items;
-  }})
-  users!: UserCollection;
+  /* Adds a type hinted prop with a default value */
+  // @Prop({default: () => new User('', '')})
+  // user!: User;
 }
 </script>
