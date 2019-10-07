56cfd67a4ffbc1a252a0a27f1720fd89be67b04b
Liam Maclachlan <dev_lime@protonmail.com>
2019-10-07 21:19:27 +0100

    - minor changes

--- src/classes/models/Post.ts
+++ src/classes/models/Post.ts
@@ -4,23 +4,15 @@ import {Item} from '@vuex-orm/core/lib/data';
 
 export default class Post extends MediaAbstract {
 
-  private _title: string = '';
-  private _body: string = '';
+  private readonly _title: string = '';
+  private readonly _body: string = '';
 
   get title(): string {
     return this._title;
   }
 
-  set title(value: string) {
-    this._title = value;
-  }
-
   get body(): string {
     return this._body;
-  }
-
-  set body(value: string) {
-    this._body = value;
   }
 
   constructor(title: string, body: string, createdAt?: Date, updatedAt?: Date, author?: Item<UserEntity>) {
