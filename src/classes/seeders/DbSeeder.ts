0f7e11f665829c1114bc0f8bac7deed8d7f867b4
Liam Maclachlan <dev_lime@protonmail.com>
2019-10-07 10:17:13 +0100

    - refactored seeder class to ensure users are available when posts seed

--- src/classes/seeders/DbSeeder.ts
+++ src/classes/seeders/DbSeeder.ts
@@ -10,37 +10,49 @@ import PostEntity from '@/classes/entities/PostEntity';
 
 export default class DbSeeder {
   public static init() {
+    DbSeeder.seedUsers(7);
+    DbSeeder.seedPosts(20);
+  }
+
+  private static seedUsers(amount: number = 5) {
     // Seed users
     UserEntity.insert({
-      data: new UserEntityFactory(DbSeeder.getUserCollection()).items,
-    });
-
-    // Seed posts
-    PostEntity.insert({
-      data: new PostEntityFactory(DbSeeder.getFormattedPosts()).items,
+      data: new UserEntityFactory(DbSeeder.getRandomUserCollection(amount)).items,
     });
   }
 
-  private static getUserCollection(): UserCollection {
+  private static seedPosts(amount: number = 5) {
+    // Seed posts
+    PostEntity.insert({
+      data: new PostEntityFactory(DbSeeder.getRandomPostCollection(amount)).items,
+    });
+  }
+
+  private static getRandomUserCollection(amount: number = 5): UserCollection {
     const items = [];
 
-    for ( let i = 0; i < 7; i += 1) {
+    for ( let i = 0; i < amount; i += 1) {
       items.push(new User(faker.name.findName() , faker.internet.email()));
     }
 
     return new UserCollection(items);
   }
 
-  private static getFormattedPosts(): PostCollection {
+  private static getRandomPostCollection(amount: number = 5): PostCollection {
+    // Users must be present in the DB to populate posts
+    if ( !UserEntity.query().first() ) {
+      DbSeeder.seedUsers(1);
+    }
+
     const authors = UserEntity.all();
     const items = [];
 
-    for ( let i = 0; i < 20; i += 1) {
+    for ( let i = 0; i < amount; i += 1) {
       const createdAt = faker.date.between(new Date('2018/01/01 GMT'), new Date(Date.now()));
       const updatedAt = faker.date.between(createdAt, new Date(Date.now()));
 
       items.push( new Post(
-        faker.lorem.words(Math.floor((Math.random() * 15) + 1)), // title
+        faker.lorem.words(Math.floor((Math.random() * 13) + 3)), // title
         faker.lorem.paragraphs(5), // body
         createdAt, // created at
         updatedAt, // updated at
