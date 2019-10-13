599300876703c3c29bc25af416f7f592c259cf70
Liam Maclachlan <dev_lime@protonmail.com>
2019-10-13 21:46:48 +0100

    - change to author type hint

--- src/classes/models/interfaces/AuthorPropsInterface.ts
+++ src/classes/models/interfaces/AuthorPropsInterface.ts
@@ -1,5 +1,7 @@
 import PropsInterface from '@/classes/models/interfaces/PropsInterface';
+import {Item} from '@vuex-orm/core/lib/data';
+import UserEntity from '@/classes/entities/UserEntity';
 
 export default interface AuthorPropsInterface extends PropsInterface {
-  author: number;
+  author: Item<UserEntity>;
 }
--- src/classes/models/traits/Author.ts
+++ src/classes/models/traits/Author.ts
@@ -1,13 +1,15 @@
 import AuthorPropsInterface from '@/classes/models/interfaces/AuthorPropsInterface';
+import UserEntity from '@/classes/entities/UserEntity';
+import {Item} from '@vuex-orm/core/lib/data';
 
 export default class Author {
-  private readonly _author: number;
+  private readonly _author: Item<UserEntity>;
 
   constructor(options: AuthorPropsInterface) {
     this._author = options.author;
   }
 
-  get author(): number {
+  get author(): Item<UserEntity> {
     return this._author;
   }
 }
