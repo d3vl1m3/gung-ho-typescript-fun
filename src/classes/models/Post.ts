25fad76e4029647d914f399c3284005334f2466c
Liam Maclachlan <dev_lime@protonmail.com>
2019-10-13 21:46:56 +0100

    - added generate random functionality in to the post model

--- src/classes/models/Post.ts
+++ src/classes/models/Post.ts
@@ -1,7 +1,27 @@
+import faker from 'faker';
 import MediaMixin from '@/classes/models/mixins/MediaMixin';
 import PostPropsInterface from '@/classes/models/interfaces/PostPropsInterface';
+import UserEntity from '@/classes/entities/UserEntity';
 
 export default class Post extends MediaMixin {
+
+  public static random(): Post {
+    return new Post(this.randomOptions());
+  }
+
+  public static randomOptions(): PostPropsInterface {
+    const createdAt = faker.date.between(new Date('2018/01/01 GMT'), new Date(Date.now()));
+    const updatedAt = faker.date.between(createdAt, new Date(Date.now()));
+    const authors = UserEntity.all();
+    return {
+      title: faker.lorem.words(Math.floor((Math.random() * 13) + 3)),
+      body: faker.lorem.paragraphs(5),
+      createdAt, // created at
+      updatedAt, // updated at
+      author: getRandomItem(authors),
+    };
+  }
+
   private readonly _title: string;
   private readonly _body: string;
 
@@ -18,5 +38,8 @@ export default class Post extends MediaMixin {
     this._title = options.title;
     this._body = options.body;
   }
+}
 
+function getRandomItem(a: any[]) {
+  return a[Math.floor(Math.random() * a.length)];
 }
--- src/classes/services/PostCollectionService.ts
+++ src/classes/services/PostCollectionService.ts
@@ -1,29 +1,12 @@
 import CollectionServiceAbstract from '@/classes/services/abstracts/CollectionServiceAbstract';
-import faker from 'faker';
 import Post from '@/classes/models/Post';
-import UserEntity from '@/classes/entities/UserEntity';
 
 export default class PostCollection extends CollectionServiceAbstract<Post> {
   protected generate(amount: number): Post[] {
-    const authors = UserEntity.all();
     const items = [];
     for ( let i = 0; i < amount; i += 1) {
-      const createdAt = faker.date.between(new Date('2018/01/01 GMT'), new Date(Date.now()));
-      const updatedAt = faker.date.between(createdAt, new Date(Date.now()));
-
-      items.push( new Post({
-        title: faker.lorem.words(Math.floor((Math.random() * 13) + 3)),
-        body: faker.lorem.paragraphs(5),
-        createdAt, // created at
-        updatedAt, // updated at
-        author: getRandomItem(authors),
-      }));
+      items.push( Post.random() );
     }
-
     return items;
   }
-}
-
-function getRandomItem(a: any[]) {
-  return a[Math.floor(Math.random() * a.length)];
 }
